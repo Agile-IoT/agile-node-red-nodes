f40cab4e576e70f122ba64bc775d9cfee04be3e9
koustabhdolui <koustabh.here@gmail.com>
2017-11-24 15:20:27 +0100

    Update execute.html to include list of commands
    Signed-off-by: koustabhdolui <koustabh.here@gmail.com>

--- device/execute.js
+++ device/execute.js
@@ -6,71 +6,71 @@ module.exports = function (RED) {
   RED.nodes.registerType('agile-device-execute', function (config) {
 
     RED.nodes.createNode(this, config)
-
-    this.server = RED.nodes.getNode(config.server)
-
-    this.name = config.name
-    this.deviceId = config.deviceId
-
-    var node = this
-    var msg = {}
-
-    d('API hostname %s:%s', this.server.host, this.server.port)
-    client.get(this.server.host + ':' + this.server.port)
-      .then(function (client) {
-
-        d('Loaded Swagger client')
-
-        var exec = function (args) {
-
-          d('Sending command..')
-
-          var promise
-          try {
-            promise = client.Device.Execute({
-              deviceId: node.deviceId,
-              command: args.command
-            })
-              .then(function (r) {
-
-                d('Got response: %j', r)
-                msg = {
-                  payload: r.data
-                }
-
-                node.send(msg)
-              })
-              .catch(function (e) {
-
-                d('Failed request %j', e)
-
-                var msg = ''
-                if(e.errObj) {
-                  msg = e.method + ' ' + e.url + ': ' + e.errObj.status + ' ' + e.data
-                }
-
-                node.error('Error executing commnad ' + node.command + ' : ' + msg)
-              })
-          } catch(e) {
-            d('Catch error %j', e)
-          }
-          return promise
-        }
-
-        node.on('input', function (msg) {
-          if(!msg.command) {
-            node.error('Cannot execute command: msg.command not set')
-            return
-          }
-          node.log('Executing ' + msg.command)
-          exec(msg)
-        })
-
-
-      })
-      .fail(function (e) {
-        node.error('Error loading Swagger client: ' + e.message)
-      })
+    d('Node was created')
+    // this.server = RED.nodes.getNode(config.server)
+    //
+    // this.name = config.name
+    // this.deviceId = config.deviceId
+    //
+    // var node = this
+    // var msg = {}
+    //
+    // d('API hostname %s:%s', this.server.host, this.server.port)
+    // client.get(this.server.host + ':' + this.server.port)
+    //   .then(function (client) {
+    //
+    //     d('Loaded Swagger client')
+    //
+    //     var exec = function (args) {
+    //
+    //       d('Sending command..')
+    //
+    //       var promise
+    //       try {
+    //         promise = client.Device.Execute({
+    //           deviceId: node.deviceId,
+    //           command: args.command
+    //         })
+    //           .then(function (r) {
+    //
+    //             d('Got response: %j', r)
+    //             msg = {
+    //               payload: r.data
+    //             }
+    //
+    //             node.send(msg)
+    //           })
+    //           .catch(function (e) {
+    //
+    //             d('Failed request %j', e)
+    //
+    //             var msg = ''
+    //             if(e.errObj) {
+    //               msg = e.method + ' ' + e.url + ': ' + e.errObj.status + ' ' + e.data
+    //             }
+    //
+    //             node.error('Error executing commnad ' + node.command + ' : ' + msg)
+    //           })
+    //       } catch(e) {
+    //         d('Catch error %j', e)
+    //       }
+    //       return promise
+    //     }
+    //
+    //     node.on('input', function (msg) {
+    //       if(!msg.command) {
+    //         node.error('Cannot execute command: msg.command not set')
+    //         return
+    //       }
+    //       node.log('Executing ' + msg.command)
+    //       exec(msg)
+    //     })
+    //
+    //
+    //   })
+    //   .fail(function (e) {
+    //     node.error('Error loading Swagger client: ' + e.message)
+    //   })
 
   })
 
