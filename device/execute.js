e7201436793e759b11de5e892c3b8bde9a35cbee
koustabhdolui <koustabh.here@gmail.com>
2017-11-24 16:01:25 +0100

    Update execute.js, execution functional on SensorTag
    Signed-off-by: koustabhdolui <koustabh.here@gmail.com>

--- device/execute.js
+++ device/execute.js
@@ -6,71 +6,74 @@ module.exports = function (RED) {
   RED.nodes.registerType('agile-device-execute', function (config) {
 
     RED.nodes.createNode(this, config)
-    d('Node was created')
-    // this.server = RED.nodes.getNode(config.server)
-    //
-    // this.name = config.name
-    // this.deviceId = config.deviceId
-    //
-    // var node = this
-    // var msg = {}
-    //
-    // d('API hostname %s:%s', this.server.host, this.server.port)
-    // client.get(this.server.host + ':' + this.server.port)
-    //   .then(function (client) {
-    //
-    //     d('Loaded Swagger client')
-    //
-    //     var exec = function (args) {
-    //
-    //       d('Sending command..')
-    //
-    //       var promise
-    //       try {
-    //         promise = client.Device.Execute({
-    //           deviceId: node.deviceId,
-    //           command: args.command
-    //         })
-    //           .then(function (r) {
-    //
-    //             d('Got response: %j', r)
-    //             msg = {
-    //               payload: r.data
-    //             }
-    //
-    //             node.send(msg)
-    //           })
-    //           .catch(function (e) {
-    //
-    //             d('Failed request %j', e)
-    //
-    //             var msg = ''
-    //             if(e.errObj) {
-    //               msg = e.method + ' ' + e.url + ': ' + e.errObj.status + ' ' + e.data
-    //             }
-    //
-    //             node.error('Error executing commnad ' + node.command + ' : ' + msg)
-    //           })
-    //       } catch(e) {
-    //         d('Catch error %j', e)
-    //       }
-    //       return promise
-    //     }
-    //
-    //     node.on('input', function (msg) {
-    //       if(!msg.command) {
-    //         node.error('Cannot execute command: msg.command not set')
-    //         return
-    //       }
-    //       node.log('Executing ' + msg.command)
-    //       exec(msg)
-    //     })
-    //
-    //
-    //   })
-    //   .fail(function (e) {
-    //     node.error('Error loading Swagger client: ' + e.message)
-    //   })
+
+    this.server = RED.nodes.getNode(config.server)
+
+    this.name = config.name
+    this.deviceId = config.deviceId
+    this.commandId = config.commandId
+
+    var node = this
+    var msg = {}
+
+    d('API hostname %s:%s', this.server.host, this.server.port)
+    client.get(this.server.host + ':' + this.server.port)
+      .then(function (client) {
+
+        d('Loaded Swagger client')
+
+        var exec = function () {
+
+          d('Sending command..')
+
+          var promise
+          try {
+            promise = client.Device.Execute({
+              deviceId: node.deviceId,
+              command: node.commandId
+            })
+              .then(function (r) {
+
+                d('Got response: %j', r)
+                msg = {
+                  payload: r.data
+                }
+
+                node.send(msg)
+              })
+              .catch(function (e) {
+
+                d('Failed request %j', e)
+
+                var msg = ''
+                if(e.errObj) {
+                  msg = e.method + ' ' + e.url + ': ' + e.errObj.status + ' ' + e.data
+                }
+
+                node.error('Error executing commnad ' + node.command + ' : ' + msg)
+              })
+          } catch(e) {
+            d('Catch error %j', e)
+          }
+          return promise
+        }
+
+        exec();
+
+        // node.on('input', function (msg) {
+        //   if(!msg.command) {
+        //     node.error('Cannot execute command: msg.command not set')
+        //     return
+        //   }
+        //   node.log('Executing ' + msg.command)
+        //   exec(msg)
+        // })
+
+
+      })
+      .fail(function (e) {
+        node.error('Error loading Swagger client: ' + e.message)
+      })
 
   })
 
